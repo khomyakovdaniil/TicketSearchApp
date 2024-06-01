2adcedb77f090634b30c0eb21ab09e1f8ba3a2b4
KhomyakovDaniil <daniil.h@appomart.com>
2024-06-01 15:40:04 +0300

    Initial Commit

---
+++ TicketSearchApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TicketSearchApp
+//
+//  Created by  Даниил Хомяков on 01.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TicketSearchApp/TicketSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketSearchAppApp.swift
+//  TicketSearchApp
+//
+//  Created by  Даниил Хомяков on 01.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TicketSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

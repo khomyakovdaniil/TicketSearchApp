fbf9776b37c490998de91a7f8a523edf93e91e35
KhomyakovDaniil <daniil.h@appomart.com>
2024-06-04 15:52:44 +0300

    SearchSheet content part 3

--- TicketSearchApp/Views/SearchSheet.swift
+++ TicketSearchApp/Views/SearchSheet.swift
@@ -14,12 +14,7 @@ struct SearchSheet: View {
     
     // MARK: - Private constants
     private let searchViewHeight = 90.0
-    private let actionCellImageHeight = 48.0
-    private let actionCellImageWidth = 48.0
-    private let suggestionCellImageHeight = 40.0
-    private let suggestionCellImageWidth = 40.0
-    private let actionsViewHeight = 114.0
-    private let actionFontSize = 14.0
+    //    private let actionsViewHeight = 114.0
     
     // MARK: - dismissal action
     @Environment(\.dismiss) var dismiss
@@ -27,92 +22,23 @@ struct SearchSheet: View {
     // MARK: - View
     var body: some View {
         VStack(alignment: .leading) {
-                VStack {
-                    HStack {
-                        Image("planeIcon")
-                            .resizable()
-                            .frame(width: 24, height: 24)
-                        TextField("",
-                                  text: $model.departureCity,
-                                  prompt: Text(model.strings.departureCityPrompt)
-                            .foregroundColor(.gray)
-                        )
-                    }
-                    Divider()
-                        .background(Color(hex: "#9F9F9F"))
-                    HStack {
-                        Image("magnifierIcon")
-                            .resizable()
-                            .frame(width: 24, height: 24)
-                        TextField("",
-                                  text: $model.arrivalCity,
-                                  prompt: Text(model.strings.arrivalCityPrompt)
-                            .foregroundColor(.gray)
-                        )
-                        .onSubmit() {
-                            dismiss()
-                            model.userEnteredArrivalCity()
-                        }
-                        Spacer()
-                        Image(systemName: "xmark")
-                            .frame(width: 24, height: 24)
-                    }
-                }
-                .padding()
-                .frame(height: searchViewHeight) // TODO: handle size
-                .background {
-                                RoundedRectangle(cornerRadius: 16)
-                        .fill(Color(hex: "#3E3F43"))
-                                    .shadow(radius: 2, y: 2)
-                            }
-            .padding(16)
-            .background {
-                            RoundedRectangle(cornerRadius: 16)
-                    .fill(Color(hex: "#2F3035"))
-                                .shadow(radius: 2, y: 2)
-                        }
-            .padding()
-            HStack {
-                ForEach(model.actions, id: \.self.text) { action in
-                    VStack(alignment: .center) {
-                        Image(action.imagename)
-                            .resizable()
-                            .frame(width: actionCellImageWidth, height: actionCellImageHeight)
-                        Text(action.text)
-                            .font(.custom(model.fontName, size: actionFontSize))
-                            .multilineTextAlignment(.center)
-                        Spacer()
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: 114)
-                    .onTapGesture {
-                        action.action()
-                    }
-                }
-                .listStyle(.plain)
+            LocationsView(departureCity: $model.departureCity,
+                          arrivalCity: $model.arrivalCity,
+                          departureCityPrompt: model.strings.departureCityPrompt,
+                          arrivalCityPrompt: model.strings.arrivalCityPrompt) {
+                dismiss()
+                model.userEnteredArrivalCity()
             }
-//            .frame(height: scrollViewHeight)
+                          .padding()
+            HStack(spacing: 0) {
+                ForEach(model.actions, id: \.self.text) { action in
+                    ActionView(action)
+                }
+            }
+            .padding(.top)
             VStack {
                 ForEach(model.suggestions, id: \.self.text) { suggestion in
-                    VStack {
-                        HStack {
-                            Image(suggestion.imagename)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: suggestionCellImageWidth, height: suggestionCellImageHeight)
-                                .clipped()
-                            VStack(alignment: .leading) {
-                                Text(suggestion.text)
-                                    .bold() // TODO: handle text appearance
-                                Text(model.strings.popularDestination)
-                                    .foregroundColor(.gray)
-                            }
-                            Spacer()
-                        }
-                        .padding(.top, 6)
-                        Divider()
-                            .background(Color(hex: "#9F9F9F"))
-                    }
-                    .onTapGesture {
+                    SuggestionView(popularDestinationString: model.strings.popularDestination, data: suggestion) {
                         dismiss()
                         model.arrivalCity = suggestion.text
                         model.userEnteredArrivalCity()
@@ -121,13 +47,146 @@ struct SearchSheet: View {
             }
             .padding()
             .background {
-                            RoundedRectangle(cornerRadius: 16)
+                RoundedRectangle(cornerRadius: 16)
                     .fill(Color(hex: "#2F3035"))
-                                .shadow(radius: 2, y: 2)
-                        }
+                    .shadow(radius: 2, y: 2)
+            }
+            .padding(.horizontal)
             Spacer()
         }
+    }
+}
+
+fileprivate struct LocationsView: View {
+    
+    @Binding var departureCity: String
+    @Binding var arrivalCity: String
+    
+    
+    
+    private let searchViewHeight = 96.0
+    
+    let departureCityPrompt: String
+    let arrivalCityPrompt: String
+    let action: () -> Void
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Image("planeIcon")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                    .foregroundColor(.gray)
+                TextField("",
+                          text: $departureCity,
+                          prompt: Text(departureCityPrompt)
+                    .foregroundColor(.gray)
+                )
+            }
+            Divider()
+                .background(Color(hex: "#9F9F9F"))
+            HStack {
+                Image("magnifierIcon")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                TextField("",
+                          text: $arrivalCity,
+                          prompt: Text(arrivalCityPrompt)
+                    .foregroundColor(.gray)
+                )
+                .onSubmit() {
+                    action()
+                }
+                Spacer()
+                Image(systemName: "xmark")
+                    .frame(width: 24, height: 24)
+                    .onTapGesture {
+                        arrivalCity = ""
+                    }
+            }
+        }
         .padding()
+        .frame(height: searchViewHeight) // TODO: handle size
+        .background {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(Color(hex: "#2F3035"))
+                .shadow(radius: 2, y: 2)
+        }
+    }
+}
+
+fileprivate struct ActionView: View {
+    
+    private let imageName: String
+    private let text: String
+    private let action: @MainActor () -> Void
+    private let actionCellImageHeight = 48.0
+    private let actionCellImageWidth = 48.0
+    private let actionFontSize = 14.0
+    
+    init(_ data: SearchSheetViewModel.ActionData) {
+        imageName = data.imagename
+        text = data.text
+        action = data.action
+    }
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Image(imageName)
+                .resizable()
+                .frame(width: actionCellImageWidth, height: actionCellImageHeight)
+            Text(text)
+                .font(.system(size: actionFontSize)) // TODO: replace font
+                .multilineTextAlignment(.center)
+            Spacer()
+        }
+        .frame(maxWidth: .infinity, maxHeight: 114)
+        .onTapGesture {
+            action()
+        }
+    }
+}
+
+fileprivate struct SuggestionView: View {
+    
+    private let imageName: String
+    private let text: String
+    private let action: @MainActor () -> Void
+    private let popularDestinationString: String
+    private let suggestionCellImageHeight = 40.0
+    private let suggestionCellImageWidth = 40.0
+    
+    init(popularDestinationString: String, data: SearchSheetViewModel.SuggestionData, action: @escaping @MainActor () -> Void) {
+        imageName = data.imagename
+        text = data.text
+        self.action = action
+        self.popularDestinationString = popularDestinationString
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: suggestionCellImageWidth, height: suggestionCellImageHeight)
+                    .clipped()
+                    .cornerRadius(8)
+                VStack(alignment: .leading) {
+                    Text(text)
+                        .bold() // TODO: handle text appearance
+                    Text(popularDestinationString)
+                        .foregroundColor(.gray)
+                }
+                Spacer()
+            }
+            .padding(.top, 6)
+            Divider()
+                .background(Color(hex: "#9F9F9F"))
+        }
+        .onTapGesture {
+            action()
+        }
     }
 }
 

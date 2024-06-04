00463c06ac62d6412eadc739d5a5124ab0c95e53
KhomyakovDaniil <daniil.h@appomart.com>
2024-06-04 13:20:48 +0300

    HomeView content part 4

--- TicketSearchApp/Views/HomeView.swift
+++ TicketSearchApp/Views/HomeView.swift
@@ -36,39 +36,9 @@ struct HomeView: View {
                     .padding()
                 Spacer()
             }
-            HStack {
-                Image("magnifierIcon")
-                    .resizable()
-                    .frame(width: 24, height: 24)
-                    .padding(.leading, 8)
-                VStack(alignment: .leading) {
-                    TextField("",
-                              text: $model.departureCity,
-                              prompt: Text(model.strings.departureCityPrompt)
-                        .foregroundColor(.gray)
-                    )
-                    Divider()
-                        .background(Color(hex: "#9F9F9F"))
-                    Text(model.strings.arrivalCityPrompt)
-                        .foregroundColor(.gray)
-                    .onTapGesture() {
-                            model.userInititatedArrivalCityTextEdit()
-                        }
-                }
+            LocationsView(departureCity: $model.departureCity, departureCityPrompt: model.strings.departureCityPrompt, arrivalCityPrompt: model.strings.arrivalCityPrompt, searchViewHeight: searchViewHeight) {
+                model.userInititatedArrivalCityTextEdit()
             }
-            .padding(.trailing)
-                .frame(height: searchViewHeight) // TODO: handle size
-                .background {
-                                RoundedRectangle(cornerRadius: 16)
-                        .fill(Color(hex: "#3E3F43"))
-                                    .shadow(radius: 2, y: 2)
-                            }
-            .padding(16)
-            .background {
-                            RoundedRectangle(cornerRadius: 16)
-                    .fill(Color(hex: "#2F3035"))
-                                .shadow(radius: 2, y: 2)
-                        }
             .padding()
             Text(model.strings.subTitle)
                 .font(.custom(model.fontName, size: subtitleFontSize))
@@ -76,24 +46,106 @@ struct HomeView: View {
             ScrollView(.horizontal) {
                 LazyHStack {
                     ForEach(model.offers ?? [], id: \.self.id) { offer in
-                        VStack(alignment: .leading) {
-                            Image(cellImagePrompt+"\(offer.id)")
-                                .resizable()
-                                .frame(width: cellImageWidth, height: cellImageHeight)
-                            Text(offer.title)
-                            Text(offer.town)
-                            Text("\(offer.price.value)")
-                        }
+                        ConcertView(cellImagePrompt: cellImagePrompt, cellImageWidth: cellImageWidth, cellImageHeight: cellImageHeight, concert: offer)
                      }
                      .listStyle(.plain)
                 }
             }
             .frame(height: scrollViewHeight)
+            .scrollIndicators(.hidden)
             .padding(.leading)
             Spacer()
         }
         .onAppear() {
             model.getData()
+        }
+    }
+}
+
+fileprivate struct LocationsView: View {
+    
+    @Binding var departureCity: String
+    let departureCityPrompt: String
+    let arrivalCityPrompt: String
+    
+    let searchViewHeight: CGFloat
+    
+    let action: () -> Void
+    
+    var body: some View {
+        HStack {
+            Image("magnifierIcon")
+                .resizable()
+                .frame(width: 24, height: 24)
+                .padding(.leading, 8)
+            VStack(alignment: .leading) {
+                TextField("",
+                          text: $departureCity,
+                          prompt: Text(departureCityPrompt)
+                    .foregroundColor(.gray)
+                )
+                Divider()
+                    .background(Color(hex: "#9F9F9F"))
+                HStack {
+                    Text(arrivalCityPrompt)
+                        .foregroundColor(.gray)
+                    Spacer()
+                }
+                .onTapGesture() {
+                        action()
+                    }
+            }
+        }
+        .padding(.trailing)
+            .frame(height: searchViewHeight)
+            .background {
+                            RoundedRectangle(cornerRadius: 16)
+                    .fill(Color(hex: "#3E3F43"))
+                                .shadow(radius: 2, y: 2)
+                        }
+        .padding(16)
+        .background {
+                        RoundedRectangle(cornerRadius: 16)
+                .fill(Color(hex: "#2F3035"))
+                            .shadow(radius: 2, y: 2)
+                    }
+    }
+}
+
+fileprivate struct ConcertView: View {
+    
+    let cellImagePrompt: String
+    let cellImageWidth: CGFloat
+    let cellImageHeight: CGFloat
+    let id: String
+    let title: String
+    let town: String
+    let price: String
+    
+    init(cellImagePrompt: String, cellImageWidth: CGFloat, cellImageHeight: CGFloat, concert: HomeViewData.Offer) {
+        self.cellImagePrompt = cellImagePrompt
+        self.cellImageWidth = cellImageWidth
+        self.cellImageHeight = cellImageHeight
+        self.id = String(concert.id)
+        self.title = concert.title
+        self.town = concert.town
+        self.price = concert.price.value.formatted()
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Image(cellImagePrompt+id)
+                .resizable()
+                .frame(width: cellImageWidth, height: cellImageHeight)
+                .cornerRadius(16)
+            Text(title)
+                .font(.title3) // TODO: set font
+            Text(town)
+            HStack(spacing: 0) {
+                Image("concertFlightIcon")
+                    .foregroundColor(.gray)
+                Text("от " + price + " ₽")
+            }
         }
     }
 }
